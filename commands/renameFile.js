b83d5d35b72d828a6d6dae2233112415f1dec41f
Uliana <ulianav95v@gmail.com>
2025-04-28 09:10:03 -0600

    fix: add file existence check to renameFile

--- commands/renameFile.js
+++ commands/renameFile.js
@@ -2,6 +2,7 @@ import { rename } from 'fs/promises';
 import { parse, format } from 'path';
 import { catchError } from '../utils/catchError.js';
 import { getPath } from '../utils/getPath.js';
+import { pathExists } from '../utils/pathExists.js';
 
 export const renameFile = (pathAndNewName) =>
   catchError(async () => {
@@ -10,11 +11,17 @@ export const renameFile = (pathAndNewName) =>
     const match = pathAndNewName.match(/^(.+\.\w+)\s+(.+)$/);
 
     if (match) {
-      path = match[1];
+      path = getPath(match[1]);
       newName = match[2];
     }
 
-    const parsedPath = parse(getPath(path));
+    const fileExists = await pathExists(path);
+
+    if (!fileExists) {
+      throw new Error('Operation failed');
+    }
+
+    const parsedPath = parse(path);
     const { root, ext, dir } = parsedPath;
     const newPath = format({
       root,
